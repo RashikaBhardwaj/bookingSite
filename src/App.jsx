9bdaddd5ee36f88f244675cf64bb0f8b9ce8160f
RashikaBhardwaj <bhardwajrashika954@gmail.com>
2024-06-24 11:16:00 +0530

    bookingsite card data frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Signin from "./Components/Signin"
-import Signup from "./Components/Signup"
-import Slidebar from "./Components/Slidebar"
-import Navbar from "./Components/Navbar"
-import Card from "./Components/Card"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
+import Slidebar from "./Components/Slidebar";
+import BookingForm from "./Components/BookingForm";
+import BookingData from "./Components/BookingData";
+import BookingUpdate from "./Components/BookingUpdate";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/Signin" element={<Signin/>}/>
-          <Route path="/Signup" element={<Signup/>}/>
-          <Route path="/Card" element={<Card/>}/>
-          <Route path="/Navbar" element={<Navbar/>}/>
-          <Route path="/Slidebar" element={<Slidebar/>}/>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/bookingform" element={<BookingForm />} />
+          <Route path="/Bookingupdate/:id" element={<BookingUpdate />} />
+          <Route path="/Bookingdata" element={<BookingData />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/slidebar" element={<Slidebar />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,41 @@
-function Card() {
+import { useEffect, useState } from "react";
 
-    return (
-      <>
-        <h1 className="text-3xl font-bold underline">
-        CARD!
-      </h1>
-      </>
-    )
-  }
+export default function Card() {
+  let[bookingCard,setBookingCard]=useState([])
+    useEffect(()=>{
+      const fetchData=async()=>{
+      const response = await fetch("http://localhost:8000/bookingdata")
+      const jsonresponse =  await response.json()
+      setBookingCard(jsonresponse);
+    }
+    fetchData()
+    },[])
+    console.log(bookingCard);
   
-  export default Card
-  
+  return (
+    <>
+    {bookingCard.map((item,index)=>(
+
+    
+      <div className="m-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <img className="w-full" src={`http://localhost:8000/image/${item.hotelimage}`} alt="Placeholder Image" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{item.hotelname}</div>
+              <p className="text-gray-700 text-base">
+                <p>{item.hotelphone}</p>
+                <p>{item.hotelemail}</p>
+                <p>{item.hotellocation}</p>
+                <p>{item.hotelrent}</p>
+                <p>{item.hotelimage}</p>
+                
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      ))}
+    </>
+  );
+}
